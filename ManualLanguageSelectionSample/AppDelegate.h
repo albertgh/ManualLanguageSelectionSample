0fe4001de606a898917d39a4cf34e968ea085f1e
Albert Chu <albertzyk@gmail.com>
2014-11-27 16:09:17 +0800

    Initial Commit

---
+++ ManualLanguageSelectionSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ManualLanguageSelectionSample
+//
+//  Created by Albert Chu on 14/11/27.
+//  Copyright (c) 2014年 Albert Chu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ManualLanguageSelectionSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ManualLanguageSelectionSample
+//
+//  Created by Albert Chu on 14/11/27.
+//  Copyright (c) 2014年 Albert Chu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
